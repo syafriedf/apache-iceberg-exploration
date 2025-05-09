c227a7fee6608053e3f60588306f5bb0b2017452
syafriedf <syafriedwif@gmail.com>
2025-05-09 09:45:42 +0700

    update: add email account receiver for notification

--- dags/utils/mailing/notifications_send_mail.py
+++ dags/utils/mailing/notifications_send_mail.py
@@ -2,7 +2,10 @@ from airflow.utils.email import send_email
 
 def notify_failure(context):
     send_email(
-        to=context['params'].get('email', 'syafrie@analyset.com'),
+        to=[
+            context['params'].get('email', 'syafrie@analyset.com'),
+            'syafriedwif@gmail.com'  # Add another email here
+        ],
         subject=f"DAG Failed: {context['dag'].dag_id}",
         html_content=f"<h3>DAG {context['dag'].dag_id} has failed.</h3><p>Run ID: {context['dag_run'].run_id}</p>"
     )
